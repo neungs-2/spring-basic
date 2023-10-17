498d90fb987da7c2c484067186ea33ce59e53b6e
logan <gnlee95@blocko.io>
2023-10-18 04:07:01 +0900

    member 관련 테스트 코드 작성 및 현재 코드 단점 체크

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,6 +2,12 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
+    /**
+     * 현재 코드는 추상화 MemberRepository와 구체화 MemoryMemberRepository 둘 다 의존하는 중
+     * => DIP 위반
+     * 추상화에만 의존하도록 수정
+     */
+
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
