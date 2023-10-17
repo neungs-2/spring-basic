9129f2abdd383639f2a8c25f957377630d50b6a1
logan <gnlee95@blocko.io>
2023-10-18 05:17:33 +0900

    Order 테스트 코드

--- core/src/test/java/hello/core/CoreApplicationTests.java
+++ core/src/test/java/hello/core/CoreApplicationTests.java
@@ -3,6 +3,7 @@ package hello.core;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// SpringBootTest는 스프링을 띄우고 하는 테스트라 오래 걸림
 @SpringBootTest
 class CoreApplicationTests {
 
