790aede5e5e37072350a69a0d69774784c40cb0e
logan <gnlee95@blocko.io>
2023-10-18 05:08:35 +0900

    OrderApp 을 이용한 직접 실행 테스트

---
+++ core/src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,25 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+// 각 패키지 명을 딴 App 파일은 테스트 코드처럼 직접 실행해서 동작을 테스트하기 위해 만든 모듈
+public class OrderApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order);
+    }
+}
