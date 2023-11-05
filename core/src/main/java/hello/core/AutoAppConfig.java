bbda6cd359632cb470e4e94b8541921c39d20533
logan <gnlee95@gmail.com>
2023-11-06 03:03:48 +0900

    ComponentScan Filter

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,8 +6,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        // AppConfig 이전에 실습했던걸 자동 스캔 대상에서 제외하기 위해 추가한 코드
+//        basePackages = "hello.core",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+        // AppConfig 이전에 실습했던걸 자동 스캔 대상에서 제외하기 위해 추가한 코드
 )
 public class AutoAppConfig {
 }
