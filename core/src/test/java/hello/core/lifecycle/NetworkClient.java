398f5425651ce2160e29e3bcd16087ed3d11a217
logan <gnlee95@gmail.com>
2023-11-08 16:35:30 +0900

    빈 생명주기 콜백: 인터페이스 사용

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,12 +1,13 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -27,4 +28,17 @@ public class NetworkClient {
         System.out.println("close: " + url);
     }
 
+    @Override
+    // 초기화 콜백
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet: " + url);
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    // 소멸전 콜백
+    public void destroy() throws Exception {
+        disconnect();
+    }
 }
