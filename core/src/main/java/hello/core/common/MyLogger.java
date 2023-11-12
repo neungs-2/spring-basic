1f1f15500eb1bb62910c1b83e3d17616a4ee5e1c
logan <gnlee95@gmail.com>
2023-11-13 02:53:18 +0900

    웹 스코프

--- core/src/main/java/hello/core/common/MyLogger.java
+++ core/src/main/java/hello/core/common/MyLogger.java
@@ -3,6 +3,7 @@ package hello.core.common;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
@@ -10,7 +11,7 @@ import java.util.UUID;
 // Provider를 사용하지 않을 시 CoreApplication 실행 시 에러가 발생한다.
 // 이는 request 스코프 빈의 생명주기가 HTTP 요청 시 생성되기 때문에 이전에 사용할 수 없기 때문이다.
 @Component
-@Scope(value = "request")
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class MyLogger {
     private String uuid;
     private String requestURL;
--- core/src/main/java/hello/core/web/LogDemoController.java
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -11,16 +11,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
+//    private final ObjectProvider<MyLogger> myLoggerProvider;
     private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody // view 없이 문자열 그대로 응답 보내기 위함
-    public String logDemo(HttpServletRequest request) {
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
         // 이렇게 로그를 남기는 코드는 컨트롤러보다 공통 처리가 가능한 스프링 인터셉터나 서블릿 필터를 활용하는 것이 더 좋다.
         // 예제를 단순화하기 위해 컨트롤러에서 처리했다.
         String requestURL = request.getRequestURI().toString();
+//        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
         myLogger.log("controller test");
+        Thread.sleep(5000);
         logDemoService.logic("testId");
         return "OK";
     }
