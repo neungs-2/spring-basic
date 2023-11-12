37ce78c8d52fb0945b12e6dd8a35dfa11cba1a2b
logan <gnlee95@gmail.com>
2023-11-13 01:50:18 +0900

    request 스코프 예제 만들기

---
+++ core/src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,36 @@
+package hello.core.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+// Provider를 사용하지 않을 시 CoreApplication 실행 시 에러가 발생한다.
+// 이는 request 스코프 빈의 생명주기가 HTTP 요청 시 생성되기 때문에 이전에 사용할 수 없기 때문이다.
+@Component
+@Scope(value = "request")
+public class MyLogger {
+    private String uuid;
+    private String requestURL;
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message) {
+        System.out.println("[" + uuid + "]" + "[" + requestURL + "] " + message);
+    }
+
+    @PostConstruct
+    public void init() {
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "] request scope bean create: " + this);
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("[" + uuid + "] request scope bean close: " + this);
+    }
+}
---
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,27 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody // view 없이 문자열 그대로 응답 보내기 위함
+    public String logDemo(HttpServletRequest request) {
+        // 이렇게 로그를 남기는 코드는 컨트롤러보다 공통 처리가 가능한 스프링 인터셉터나 서블릿 필터를 활용하는 것이 더 좋다.
+        // 예제를 단순화하기 위해 컨트롤러에서 처리했다.
+        String requestURL = request.getRequestURI().toString();
+        myLogger.setRequestURL(requestURL);
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
